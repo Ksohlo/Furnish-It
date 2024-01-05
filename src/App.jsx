4c8c5c57da5e295418fbddaca9af075446297588
Ksohlo <alabssouls@gmail.com>
2024-01-05 14:56:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
